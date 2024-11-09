1d7ddbfd440f9ee8255ac8265d6d1cdf8395e729
PedroJSL852 <89207911+PedroJSL852@users.noreply.github.com>
2024-11-09 01:06:02 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.header('Lanzar una moneda')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
+
+$ ls -a
+.git .gitignore .streamlit app.py README.md requirements.txt
